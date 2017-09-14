6c0d400bd2ed7993ac2336bba546ff82343a9042
gondola731 <zy731@hotmail.com>
2017-09-14 20:56:57 +0800

    20170914

--- Arbitrage/smartarbitrage/raven/public_markets/_huobi.py
+++ Arbitrage/smartarbitrage/raven/public_markets/_huobi.py
@@ -6,7 +6,20 @@ import urllib.parse
 import json
 from .market import Market
 
+HOST = "https://api.huobi.com/"
+API_V3_URI = HOST + "apiv3/"
+
+KlineUrl = HOST + "%s" + "/" + "%s" + "_kline_" + "%03d" + "_json.js"
+QuotationUrl = HOST + "%s" + "/" + "ticker_%s_json.js"
+DepthUrl = HOST + "%s/depth_%s_%d.js"
+RealTimeTransactionUrl = HOST + "%s/detail_%s_json.js"
+
 class Huobi(Market):
+    accessKey = ""
+    secretKey = ""
+    market =""
+    userAgent = ""
+
     def __init__(self, base_currency, market_currency, pair_code):
         super().__init__(base_currency, market_currency, pair_code)
 
@@ -23,5 +36,37 @@ class Huobi(Market):
         res = urllib.request.urlopen(req)
         depth = json.loads(res.read().decode('utf8'))
         self.depth = self.format_depth(depth)
+
+
+    def SendRequest(self, url, parameter) :
+        # body: = ioutil.NopCloser(strings.NewReader(parameter)) // 把form数据编码
+        # req, _:= http.NewRequest("POST", uri, body)
+        # req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value") // post方式需要
+        # req.Header.Add("User-Agent", hb.userAgent)
+        # resp, err:= hb.httpClient.Do(req) // 发送请求
+        # data:= []byte{}
+        # if err != nil
+        # {
+        #     return data, err
+        # }
+        # defer
+        # resp.Body.Close() // 一定要关闭resp.Body
+        # return ioutil.ReadAll(resp.Body)
+        req = urllib.request.Request(url, headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "*/*",
+            "User-Agent": "curl/7.24.0 (x86_64-apple-darwin12.0)"})
+        res = urllib.request.urlopen(req)
+
+
+    def SendTradingRequest(self, parameter):
+        # if len(hb.accessKey) < 20 | | len(hb.secretKey) < 20
+        # {
+        #     return []
+        #         byte{}, errors.New("error！the huobi AccessKey and SecretKey is incorrect.")
+        # }
+
+        url= API_V3_URI
+        return SendRequest(self, url, parameter)
 
 
